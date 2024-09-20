16fc17ae30b98cade2ef6bbfa88f8e356dfcc388
itisAE <eugenio.armando@itiscuneo.eu>
2024-09-20 23:13:02 +0200

    correzione e compito

--- 04-socket_udp_client2.py
+++ 04-socket_udp_client2.py
@@ -5,15 +5,15 @@ import socket
 server_address = ("192.168.0.1",22222) #indirizzo ip e porta in una tupla
 BUFFER_SIZE = 4092  #numero di byte massimo che posso mandare e/o ricevere
 udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    #DGRAM significa che è udp
+NUM_INVII=10
 
-message="CIAO dal client"
 try:
-    udp_client_socket.sendto(message.encode(), server_address)
-    data, address =udp_client_socket.recvfrom(BUFFER_SIZE)
-    print(f'Messaggio ricevuto: {data.decode()} da {address}')  #decodifica dei dati
+    for i in range(NUM_INVII):
+        udp_client_socket.sendto((i+1).encode(), server_address)
 
 except OSError as e:
     print(f"ERRORE: {e}")
 
-
 udp_client_socket.close()
+
+
